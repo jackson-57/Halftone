58203f2901c5a1f7b57fc28c7d62a988805b600c
Jackson_57 <49173011+jackson-57@users.noreply.github.com>
2023-05-02 21:44:16 -0700

    Buffer Opus samples
    How does this work

--- main.c
+++ main.c
@@ -2,10 +2,18 @@
 #include "opusfile.h"
 #include "speex/speex_resampler.h"
 
+//#define OPUS_BUFFER_SIZE 11520
+#define OPUS_BUFFER_SIZE 1023
+
 static PlaydateAPI* pd = NULL;
 
-// // bigassbuffer: lib/opusfile/examples/seeking_example.c:314
-// #define BIG_ASS_BUFFER_SIZE 11520
+// Buffer struct
+typedef struct {
+    int16_t *buf;
+    int size;
+    int count;
+    int pos;
+} Buffer;
 
 // Context state for audio callback
 typedef struct {
@@ -13,13 +21,55 @@ typedef struct {
     SoundSource *source;
     OggOpusFile *of;
     SpeexResamplerState *spx;
-//    int16_t big_ass_buffer[BIG_ASS_BUFFER_SIZE];
-//    int samples_count;
+    Buffer *opus_buf;
 } AudioState;
 
 OpusFileCallbacks cb;
 SpeexResamplerState* spx;
 
+int get_opus_samples(Buffer *buffer, int len, OggOpusFile *of)
+{
+    // todo: check if length fits in buffer
+    // refill if samples in buffer is less than requested amount
+    if (len > buffer->count)
+    {
+        // move remaining data to beginning
+        // destination, source
+        memcpy(buffer->buf, buffer->buf + buffer->pos, buffer->size);
+        buffer->pos = 0;
+
+        // get samples
+        // todo: error/zero handling
+        do {
+            int offset = buffer->pos + buffer->count;
+            int result = op_read_stereo(of, buffer->buf + offset, buffer->size - offset);
+            if (result > 0)
+            {
+                buffer->count += result * 2;
+            }
+            else
+            {
+                break;
+            }
+        }
+        while (len > buffer->count);
+    }
+
+    // return
+    if (buffer->count > len)
+    {
+        buffer->count -= len;
+        buffer->pos += len;
+        return len;
+    }
+    else
+    {
+        int temp = buffer->count;
+        buffer->count = 0;
+        buffer->pos += temp;
+        return temp;
+    }
+}
 
 // Audio callback routine
 int AudioHandler(void *context, int16_t *left, int16_t *right, int len) {
@@ -30,41 +80,30 @@ int AudioHandler(void *context, int16_t *left, int16_t *right, int len) {
     //   spend as little time here as possible
 
     int target = len * 2;
-    int16_t buffer[target];
-    int result;
-    int remaining = target;
-    int total_read = 0;
-    do
-    {
-        // read opus samples. returns the number of samples read, or an error if negative
-        result = op_read_stereo(state->of, buffer + total_read, remaining);
 
-        // update total samples read if not an error, and remaining samples
-        if (result > 0)
-            total_read += result * 2;
-        remaining = target - total_read;
+    // get opus samples
+    int samples = get_opus_samples(state->opus_buf, target, state->of);
+    int16_t *buffer = state->opus_buf->buf + state->opus_buf->pos - samples;
 
-        // if no samples read or an error, stop after processing
-        if (result <= 0) {
-            if (result < 0)
-            {
-                pd->system->logToConsole("Opus error while decoding: %i", result);
-            }
+//    // if no samples read or an error, stop after processing
+//    if (result <= 0) {
+//        if (result < 0)
+//        {
+//            pd->system->logToConsole("Opus error while decoding: %i", result);
+//        }
+//
+//        op_free(state->of);
+//        state->pd->sound->removeSource(state->source);
+//        state->pd->system->realloc(state, 0);
+//        break;
+//    }
 
-            op_free(state->of);
-            state->pd->sound->removeSource(state->source);
-            state->pd->system->realloc(state, 0);
-            break;
-        }
-    }
-    while (remaining > 0);
-
-    // Resample
-    int half_read = total_read / 2;
-    int16_t resample_buffer[len * 2];
-    uint32_t in_len = half_read;
-    uint32_t out_len = len;
-    speex_resampler_process_interleaved_int(state->spx, buffer, &in_len, resample_buffer, &out_len);
+//    // Resample
+//    int half_read = total_read / 2;
+//    int16_t resample_buffer[len * 2];
+//    uint32_t in_len = half_read;
+//    uint32_t out_len = len;
+//    speex_resampler_process_interleaved_int(state->spx, buffer, &in_len, resample_buffer, &out_len);
 
 //    // Stop if resampling was unsuccessful
 //    if (out_len != half_read) {
@@ -79,10 +118,11 @@ int AudioHandler(void *context, int16_t *left, int16_t *right, int len) {
     // https://stackoverflow.com/q/14567786
     // Deinterleave stream and put into buffers
     int i, j;
-    for (i = 0, j = 0; i < out_len; i++, j += 2)
+    int half_samples = samples / 2;
+    for (i = 0, j = 0; i < half_samples; i++, j += 2)
     {
-        left[i] = resample_buffer[j];
-        right[i] = resample_buffer[j+1];
+        left[i] = buffer[j];
+        right[i] = buffer[j+1];
     }
 
     // Audio data is meaningful, so return 1
@@ -112,13 +152,22 @@ static int play_music_demo(lua_State* L)
         return 0;
     }
 
+    // create opus buffer
+    Buffer *op_buf = pd->system->realloc(NULL, sizeof(Buffer));
+//    int16_t buf[OPUS_BUFFER_SIZE];
+    int16_t *buf = pd->system->realloc(NULL, OPUS_BUFFER_SIZE * sizeof(Buffer));
+    op_buf->buf = buf;
+    op_buf->size = OPUS_BUFFER_SIZE;
+    op_buf->count = 0;
+    op_buf->pos = 0;
+
     // Create a new audio source with a state context
-    AudioState *state = pd->system->realloc(NULL, sizeof (AudioState));
+    AudioState *state = pd->system->realloc(NULL, sizeof(AudioState));
     state->pd     = pd;
     state->source = pd->sound->addSource(&AudioHandler, state, 1);
     state->of     = of;
     state->spx    = spx;
-//    state->samples_count = 0;
+    state->opus_buf = op_buf;
 
     return 1;
 }
