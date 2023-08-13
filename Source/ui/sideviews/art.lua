c05a01883e2a01483d520bb180dc96c4b663a17e
Jackson_57 <49173011+jackson-57@users.noreply.github.com>
2023-08-13 15:16:40 -0700

    Disable sideview updates

--- Source/ui/sideviews/art.lua
+++ Source/ui/sideviews/art.lua
@@ -5,6 +5,7 @@ class("ArtSideview").extends(playdate.graphics.sprite)
 function ArtSideview:init()
     ArtSideview.super.init(self)
 
+    self:setUpdatesEnabled(false)
     self:setCenter(0, 0)
     self:moveTo(consts.panel_width, 0)
     self:add()
--- Source/ui/sideviews/barsideview.lua
+++ Source/ui/sideviews/barsideview.lua
@@ -5,6 +5,7 @@ class("BarSideview").extends(playdate.graphics.sprite)
 function BarSideview:init()
     BarSideview.super.init(self)
 
+    self:setUpdatesEnabled(false)
     self:setCenter(0, 0)
     self:setBounds(consts.panel_width, consts.cover_size_full, consts.cover_size_full, consts.progress_height)
     self:add()
