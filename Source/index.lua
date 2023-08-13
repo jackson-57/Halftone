d21f10ff1b2c773c1ed4a709974872f003c5afce
Jackson_57 <49173011+jackson-57@users.noreply.github.com>
2023-08-13 00:32:48 -0700

    Remove entry names from index

--- Source/index.lua
+++ Source/index.lua
@@ -4,28 +4,38 @@ local pd_file <const> = playdate.file
 
 local debug_track_count = 0
 
-local function index_files(dir, index)
+local function index_files(dir, index, named_index)
+    if not named_index then
+        named_index = {}
+    end
+
     for _, file in pairs(pd_file.listFiles(dir)) do
         if pd_file.isdir(file) then
-            index_files(dir .. file, index)
+            index_files(dir .. file, index, named_index)
         elseif string.match(file, "[^.]+$") == "opus" then
             local path = dir .. file
             local meta_duration, meta_title, meta_album, meta_artist, meta_album_artist, meta_year, meta_track_number = index_file(path)
             if meta_duration then
                 local track = {path=path, title=meta_title, artist=meta_artist, duration = meta_duration}
 
-                local artist = index.artists[meta_album_artist]
+                local artist = nil
+                local named_artist = named_index[meta_album_artist]
                 -- create artist if not present
-                if not artist then
+                if not named_artist then
                     artist = {name = meta_album_artist, albums = {}}
-                    index.artists[meta_album_artist] = artist
+                    named_artist = {artist = artist, albums = {}}
+                    named_index[meta_album_artist] = named_artist
+                    table.insert(index.artists, artist)
+                else
+                    artist = named_artist.artist
                 end
 
-                local album = artist.albums[meta_album]
+                local album = named_artist.albums[meta_album]
                 -- create album if not present
                 if not album then
                     album = {title = meta_album, year = meta_year, tracks = {}}
-                    artist.albums[meta_album] = album
+                    named_artist.albums[meta_album] = album
+                    table.insert(artist.albums, album)
                 end
 
                 -- table.insert(album.tracks, meta_track_number, track)
